cf5c4dc2301f47139b194b985bed9c4bfb092595
Luther Tchofo Safo <nintendobserver@gmail.com>
2024-03-12 16:42:57 +0100

    Requests and notifications loader made.

--- app/routes/users.$username.requests.tsx
+++ app/routes/users.$username.requests.tsx
@@ -3,20 +3,37 @@ import { useLoaderData } from "@remix-run/react";
 
 import { H1 } from "~/components/h1";
 import { PageLink } from "~/components/page-link";
+import {
+  findSentFriendToContactsByUserId,
+  findSentIrlToContactsByUserId,
+} from "~/librairies/data/contacts";
+import { findUserByUsername } from "~/librairies/data/users";
 
 export const loader = async ({ params }: LoaderFunctionArgs) => {
-  const username = params.username;
-  return { username };
+  const user = await findUserByUsername(params.username!);
+  if (!user) {
+    throw new Response("Could not find requested user.", {
+      status: 404,
+    });
+  }
+
+  const [sentFriendToContacts, sentIrlToContacts] = await Promise.all([
+    findSentFriendToContactsByUserId(user.id),
+    findSentIrlToContactsByUserId(user.id),
+  ]);
+
+  return { user, sentFriendToContacts, sentIrlToContacts };
 };
 
 export default function RequestsPage() {
   const data = useLoaderData<typeof loader>();
+  console.log(data);
 
   return (
     <>
-      <H1>Welcome to {data.username}&apos;s Requests.</H1>
+      <H1>Welcome to {data.user.appWideName}&apos;s Requests.</H1>
 
-      <PageLink href={`/users/${data.username}/dashboard`}>
+      <PageLink href={`/users/${data.user.username}/dashboard`}>
         back to dashboard (for now)
       </PageLink>
 
