0aaffe107da621c20c9ff882eb6c11a80f0846e3
Luther Tchofo Safo <nintendobserver@gmail.com>
2024-04-14 18:01:11 +0200

    Remade the backgrounds in Tailwind.

--- app/components/main.tsx
+++ app/components/main.tsx
@@ -1,9 +1,11 @@
 export function Main({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <main className="flex min-h-screen w-full items-center justify-center px-8 py-24">
+      <main className="flex min-h-screen w-full items-center justify-center px-8 py-24 text-black dark:text-white bg-gradient-to-b from-transparent to-white dark:to-black bg-[#D6DBDC] dark:bg-[#080C14]">
         {children}
       </main>
     </>
   );
 }
+// #d6dbdc // #D6DBDC
+// #080c14 // #080C14
